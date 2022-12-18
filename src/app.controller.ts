4579e38d4f4f597b958cffe43e638c5ba8dda401
PabloMelo <pablomelodev@gmail.com>
2022-12-17 21:36:49 -0300

    feat: initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('app')
+export class AppController {
+  @Get('hello')
+  async getHello(): Promise<string> {
+    return 'hello';
+  }
+}
