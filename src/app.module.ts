0ab4826d362cdfc842700a409ef8daccdac6fca8
PabloMelo <pablomelodev@gmail.com>
2022-12-18 00:25:16 -0300

    feat: create domain

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [PrismaService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
